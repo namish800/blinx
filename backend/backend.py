7229b06b75f7e95932da305ffe20e169ca411707
tanmaydarmorha <tanmaydarmorha@gmail.com>
2024-09-17 23:57:37 +0530

    move requirements.txt to root folder

---
+++ backend/backend.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = False
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"item_id": "123", "item": item}
