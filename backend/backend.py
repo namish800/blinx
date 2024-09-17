8688d830cd13a704dcb3f92ca2712db07a1ba07b
tanmaydarmorha <tanmaydarmorha@gmail.com>
2024-09-18 00:24:57 +0530

    setup backend infra

--- backend/backend.py
+++ backend/backend.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = False
+@app.get("/hello")
+async def root():
+    return {"message": "Welcome to your FastAPI Dockerized backend!"}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return {"item_id": "123", "item": item}
+
+#   
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
